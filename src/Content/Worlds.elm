e1c0fb8c6aae064f4d97c919fe00a3486f8578b5
Isak <isakulstrup@gmail.com>
2023-09-25 19:18:21 +0200

    made rope scene a bit prettier

--- src/Content/Worlds.elm
+++ src/Content/Worlds.elm
@@ -12,13 +12,31 @@ rope : World
 rope =
     World.empty
         |> World.addForce (Absolute <| Vector2.new 0 100) True
-        |> World.addParticle (Particle.new (Vector2.new 0 -70) 0)
-        |> World.addParticle (Particle.new (Vector2.new 50 0) 1)
-        |> World.addParticle (Particle.new (Vector2.new -50 70) 1)
-        |> World.addParticle (Particle.new (Vector2.new 30 100) 1)
-        |> World.addAutoSpring 0 1 1000 1000
-        |> World.addAutoSpring 1 2 1000 10
-        |> World.addAutoSpring 2 3 1000 10
+        |> World.addParticle (Particle.new (Vector2.new -180 0) 0)
+        |> World.addParticle (Particle.new (Vector2.new -150 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new -120 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new -90 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new -60 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new -30 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 0 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 30 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 60 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 90 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 120 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 150 0) 1)
+        |> World.addParticle (Particle.new (Vector2.new 180 0) 1)
+        |> World.addAutoSpring 0 1 1000 100
+        |> World.addAutoSpring 1 2 1000 100
+        |> World.addAutoSpring 2 3 1000 100
+        |> World.addAutoSpring 3 4 1000 100
+        |> World.addAutoSpring 4 5 1000 100
+        |> World.addAutoSpring 5 6 1000 100
+        |> World.addAutoSpring 6 7 1000 100
+        |> World.addAutoSpring 7 8 1000 100
+        |> World.addAutoSpring 8 9 1000 100
+        |> World.addAutoSpring 9 10 1000 100
+        |> World.addAutoSpring 10 11 1000 100
+        |> World.addAutoSpring 11 12 1000 100
 
 
 ring : World
