d17ec7b27fa6ec892171a3013b34a659d4d693ed
Isak <isakulstrup@gmail.com>
2023-09-26 10:07:38 +0200

    added render module

---
+++ src/ParticleEngine/Render.elm
@@ -0,0 +1,51 @@
+module ParticleEngine.Render exposing
+    ( RenderConfig
+    , setHeight
+    , setWidth
+    , viewWorld
+    )
+
+import ParticleEngine.World exposing (World)
+import Svg exposing (Svg)
+import Svg.Attributes
+
+
+type alias RenderConfig =
+    { width : Float
+    , height : Float
+    }
+
+
+setWidth : Float -> RenderConfig -> RenderConfig
+setWidth width config =
+    { config | width = width }
+
+
+setHeight : Float -> RenderConfig -> RenderConfig
+setHeight height config =
+    { config | height = height }
+
+
+viewBox : RenderConfig -> Svg.Attribute msg
+viewBox config =
+    Svg.Attributes.viewBox <|
+        String.fromFloat -(config.width / 2)
+            ++ " "
+            ++ String.fromFloat -(config.height / 2)
+            ++ " "
+            ++ String.fromFloat config.width
+            ++ " "
+            ++ String.fromFloat config.height
+
+
+viewWorld : (renderSystem -> Svg msg) -> RenderConfig -> World renderSystem -> Svg msg
+viewWorld runRenderSystem config world =
+    Svg.svg
+        [ viewBox config
+        , Svg.Attributes.id "game-view"
+        ]
+        (world.renderSystems
+            |> List.filter Tuple.first
+            |> List.map Tuple.second
+            |> List.map runRenderSystem
+        )
